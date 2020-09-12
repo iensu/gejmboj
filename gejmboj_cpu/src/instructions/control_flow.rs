8d1f722b7ccc4d00546e6f738e3e98e7e99e5880
Jens Östlund <jostlund@gmail.com>
2020-09-12 10:44:05 +0200

    Refactor all instructions to be defined by `define_instruction`

--- gejmboj_cpu/src/instructions/control_flow.rs
+++ gejmboj_cpu/src/instructions/control_flow.rs
@@ -68,130 +68,90 @@ define_instruction! {
     }
 }
 
-/// Unconditional jump to location specified by register HL
-///
-/// ## Examples
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// let instruction = JpToHL { };
-///
-/// registers.set_double(DoubleRegister::HL, 0xBADA);
-/// instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-/// assert_eq!(0xBADA, registers.PC);
-/// ```
-pub struct JpToHL {}
+define_instruction! {
+    /// Unconditional jump to location specified by register HL
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// let instruction = JpToHL { };
+    ///
+    /// registers.set_double(DoubleRegister::HL, 0xBADA);
+    /// instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    /// assert_eq!(0xBADA, registers.PC);
+    /// ```
+    JpToHL { "JP(HL)"; 1 }
 
-impl Instruction for JpToHL {
-    fn execute(
-        &self,
-        registers: &mut Registers,
-        _memory: &mut Memory,
-        _cpu_flags: &mut CpuFlags,
-    ) -> InstructionResult {
+    (self, registers) => {
         registers.PC = registers.get_double(DoubleRegister::HL);
         Ok(1)
     }
-
-    fn length(&self) -> u16 {
-        1
-    }
 }
 
-impl Display for JpToHL {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "JP(HL)")
-    }
-}
+define_instruction! {
+    /// Unconditional jump to location at current + offset
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// registers.PC = 0x0200;
+    /// let instruction = JpToOffset { operand: 0x42 };
+    ///
+    /// instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    /// assert_eq!(0x0242, registers.PC);
+    /// ```
+    JpToOffset { "JP(PC + {:02x})", operand: u8; 2 }
 
-/// Unconditional jump to location at current + offset
-///
-/// ## Examples
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// registers.PC = 0x0200;
-/// let instruction = JpToOffset { operand: 0x42 };
-///
-/// instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-/// assert_eq!(0x0242, registers.PC);
-/// ```
-pub struct JpToOffset {
-    pub operand: u8,
-}
-
-impl Instruction for JpToOffset {
-    fn execute(
-        &self,
-        registers: &mut Registers,
-        _memory: &mut Memory,
-        _cpu_flags: &mut CpuFlags,
-    ) -> InstructionResult {
+    (self, registers) => {
         registers.PC += self.operand as u16;
         Ok(3)
     }
-
-    fn length(&self) -> u16 {
-        2
-    }
 }
 
-impl Display for JpToOffset {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "JP(PC + {:02x})", self.operand)
-    }
-}
+define_instruction! {
+    /// Conditional jump to relative address specified by offset operand.
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// let instruction = JpToOffsetIf { operand: 0x42, condition: Condition::Zero };
+    /// let mut cycles = 0;
+    /// registers.PC = 0x0200;
+    ///
+    /// cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    /// assert_eq!(0x0200, registers.PC);
+    /// assert_eq!(2, cycles);
+    ///
+    /// registers.set_single(SingleRegister::F, 0b1000_0000);
+    ///
+    /// cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    /// assert_eq!(0x0242, registers.PC);
+    /// assert_eq!(3, cycles);
+    /// ```
+    JpToOffsetIf { "JP(PC + {:02x}) {:?}", operand: u8, condition: Condition; 2 }
 
-/// Coonditional jump to relative address specified by offset operand.
-///
-/// ## Examples
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// let instruction = JpToOffsetIf { operand: 0x42, condition: Condition::Zero };
-/// let mut cycles = 0;
-/// registers.PC = 0x0200;
-///
-/// cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-/// assert_eq!(0x0200, registers.PC);
-/// assert_eq!(2, cycles);
-///
-/// registers.set_single(SingleRegister::F, 0b1000_0000);
-///
-/// cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-/// assert_eq!(0x0242, registers.PC);
-/// assert_eq!(3, cycles);
-/// ```
-pub struct JpToOffsetIf {
-    pub operand: u8,
-    pub condition: Condition,
-}
-
-impl Instruction for JpToOffsetIf {
-    fn execute(
-        &self,
-        registers: &mut Registers,
-        _memory: &mut Memory,
-        _cpu_flags: &mut CpuFlags,
-    ) -> InstructionResult {
+    (self, registers) => {
         if self.condition.is_fulfilled(registers) {
             registers.PC += self.operand as u16;
             Ok(3)
@@ -199,49 +159,32 @@ impl Instruction for JpToOffsetIf {
             Ok(2)
         }
     }
-
-    fn length(&self) -> u16 {
-        2
-    }
 }
 
-impl Display for JpToOffsetIf {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "JP(PC + {:02x}) {:?}", self.operand, self.condition)
-    }
-}
+define_instruction! {
+    /// Unconditional call of the function at operand address.
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// let instruction = Call { operand: 0xABCD };
+    /// registers.PC = 0xAAAA;
+    /// instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    ///
+    /// assert_eq!(0xABCD, registers.PC);
+    /// assert_eq!(0xFFFC, registers.SP);
+    /// assert_eq!(0xAAAA, memory.get_u16(registers.SP.into()));
+    /// ```
+    Call { "CALL({:04x})", operand: u16; 3 }
 
-/// Unconditional call of the function at operand address.
-///
-/// ## Examples
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// let instruction = Call { operand: 0xABCD };
-/// registers.PC = 0xAAAA;
-/// instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-///
-/// assert_eq!(0xABCD, registers.PC);
-/// assert_eq!(0xFFFC, registers.SP);
-/// assert_eq!(0xAAAA, memory.get_u16(registers.SP.into()));
-/// ```
-pub struct Call {
-    pub operand: u16,
-}
-
-impl Instruction for Call {
-    fn execute(
-        &self,
-        registers: &mut Registers,
-        memory: &mut Memory,
-        _cpu_flags: &mut CpuFlags,
-    ) -> InstructionResult {
+    (self, registers, memory) => {
         let [lo, hi] = registers.PC.to_le_bytes();
         memory.set((registers.SP - 1).into(), hi);
         memory.set((registers.SP - 2).into(), lo);
@@ -249,74 +192,56 @@ impl Instruction for Call {
         registers.PC = self.operand;
         Ok(6)
     }
-
-    fn length(&self) -> u16 {
-        3
-    }
 }
 
-impl Display for Call {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "CALL({:04x})", self.operand)
-    }
-}
+define_instruction! {
+    /// Conditional function call.
+    ///
+    /// ## Examples
+    ///
+    /// Function is not called if the condition is not fulfilled:
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// let instruction = CallIf { operand: 0xABCD, condition: Condition::Carry };
+    /// registers.PC = 0xAAAA;
+    ///
+    /// let cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    ///
+    /// assert_eq!(registers.PC, 0xAAAA);
+    /// assert_eq!(registers.SP, 0xFFFE);
+    /// assert_eq!(3, cycles);
+    /// ```
+    ///
+    /// If the condition is fulfilled then the function is called:
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// let instruction = CallIf { operand: 0xABCD, condition: Condition::Carry };
+    /// registers.PC = 0xAAAA;
+    /// registers.set_single(SingleRegister::F, 0b0001_0000);
+    ///
+    /// let cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    ///
+    /// assert_eq!(registers.PC, 0xABCD);
+    /// assert_eq!(registers.SP, 0xFFFC);
+    /// assert_eq!(6, cycles);
+    /// ```
+    CallIf { "CALL({:04x}) {:?}", operand: u16, condition: Condition; 3 }
 
-/// Conditional function call.
-///
-/// ## Examples
-///
-/// Function is not called if the condition is not fulfilled:
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// let instruction = CallIf { operand: 0xABCD, condition: Condition::Carry };
-/// registers.PC = 0xAAAA;
-///
-/// let cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-///
-/// assert_eq!(registers.PC, 0xAAAA);
-/// assert_eq!(registers.SP, 0xFFFE);
-/// assert_eq!(3, cycles);
-/// ```
-///
-/// If the condition is fulfilled then the function is called:
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// let instruction = CallIf { operand: 0xABCD, condition: Condition::Carry };
-/// registers.PC = 0xAAAA;
-/// registers.set_single(SingleRegister::F, 0b0001_0000);
-///
-/// let cycles = instruction.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-///
-/// assert_eq!(registers.PC, 0xABCD);
-/// assert_eq!(registers.SP, 0xFFFC);
-/// assert_eq!(6, cycles);
-/// ```
-pub struct CallIf {
-    pub operand: u16,
-    pub condition: Condition,
-}
-
-impl Instruction for CallIf {
-    fn execute(
-        &self,
-        registers: &mut Registers,
-        memory: &mut Memory,
-        _cpu_flags: &mut CpuFlags,
-    ) -> InstructionResult {
+    (self, registers, memory) => {
         if self.condition.is_fulfilled(registers) {
             let [lo, hi] = registers.PC.to_le_bytes();
             memory.set((registers.SP - 1).into(), hi);
@@ -328,110 +253,78 @@ impl Instruction for CallIf {
             Ok(3)
         }
     }
-
-    fn length(&self) -> u16 {
-        3
-    }
 }
 
-impl Display for CallIf {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "CALL({:04x}) {:?}", self.operand, self.condition)
-    }
-}
+define_instruction! {
+    /// Unconditional return from function.
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// let function_call = Call { operand: 0xABCD };
+    /// let return_call = Ret { };
+    ///
+    /// registers.PC = 0xAAAA;
+    /// function_call.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    /// return_call.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    ///
+    /// assert_eq!(0xAAAA, registers.PC);
+    /// assert_eq!(0xFFFE, registers.SP);
+    /// ```
+    Ret { "RET"; 1 }
 
-/// Unconditional return from function.
-///
-/// ## Examples
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// let function_call = Call { operand: 0xABCD };
-/// let return_call = Ret { };
-///
-/// registers.PC = 0xAAAA;
-/// function_call.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-/// return_call.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-///
-/// assert_eq!(0xAAAA, registers.PC);
-/// assert_eq!(0xFFFE, registers.SP);
-/// ```
-pub struct Ret {}
-
-impl Instruction for Ret {
-    fn execute(
-        &self,
-        registers: &mut Registers,
-        memory: &mut Memory,
-        _cpu_flags: &mut CpuFlags,
-    ) -> InstructionResult {
+    (self, registers, memory) => {
         registers.PC = memory.get_u16(registers.SP.into());
         registers.SP += 2;
         Ok(4)
     }
-
-    fn length(&self) -> u16 {
-        1
-    }
 }
 
-impl Display for Ret {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "RET")
-    }
-}
+define_instruction! {
+    /// Conditionally return from function.
+    ///
+    /// ## Examples
+    ///
+    /// Return from function call only if condition is fulfilled:
+    ///
+    /// ```
+    /// # use gejmboj_cpu::registers::*;
+    /// # use gejmboj_cpu::memory::*;
+    /// # use gejmboj_cpu::instructions::*;
+    /// # use gejmboj_cpu::cpu::*;
+    /// let mut registers = Registers::new();
+    /// let mut memory = Memory::new();
+    /// let mut cpu_flags = CpuFlags::new();
+    /// registers.PC = 0xAAAA;
+    ///
+    /// let call = Call { operand: 0xABCD };
+    /// call.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    ///
+    /// let ret = RetIf { condition: Condition::Carry };
+    /// let mut cycles = 0;
+    /// cycles = ret.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    ///
+    /// assert_eq!(0xABCD, registers.PC);
+    /// assert_eq!(0xFFFC, registers.SP);
+    /// assert_eq!(2, cycles);
+    ///
+    /// registers.set_single(SingleRegister::F, 0b0001_0000);
+    /// cycles = ret.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
+    ///
+    /// assert_eq!(0xAAAA, registers.PC);
+    /// assert_eq!(0xFFFE, registers.SP);
+    /// assert_eq!(5, cycles);
+    /// ```
+    RetIf { "RET {:?}", condition: Condition; 1 }
 
-/// Conditionally return from function.
-///
-/// ## Examples
-///
-/// Return from function call only if condition is fulfilled:
-///
-/// ```
-/// # use gejmboj_cpu::registers::*;
-/// # use gejmboj_cpu::memory::*;
-/// # use gejmboj_cpu::instructions::*;
-/// # use gejmboj_cpu::cpu::*;
-/// let mut registers = Registers::new();
-/// let mut memory = Memory::new();
-/// let mut cpu_flags = CpuFlags::new();
-/// registers.PC = 0xAAAA;
-///
-/// let call = Call { operand: 0xABCD };
-/// call.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-///
-/// let ret = RetIf { condition: Condition::Carry };
-/// let mut cycles = 0;
-/// cycles = ret.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-///
-/// assert_eq!(0xABCD, registers.PC);
-/// assert_eq!(0xFFFC, registers.SP);
-/// assert_eq!(2, cycles);
-///
-/// registers.set_single(SingleRegister::F, 0b0001_0000);
-/// cycles = ret.execute(&mut registers, &mut memory, &mut cpu_flags).unwrap();
-///
-/// assert_eq!(0xAAAA, registers.PC);
-/// assert_eq!(0xFFFE, registers.SP);
-/// assert_eq!(5, cycles);
-/// ```
-pub struct RetIf {
-    pub condition: Condition,
-}
-
-impl Instruction for RetIf {
-    fn execute(
-        &self,
-        registers: &mut Registers,
-        memory: &mut Memory,
-        _cpu_flags: &mut CpuFlags,
-    ) -> InstructionResult {
+    (self, registers, memory) => {
         if self.condition.is_fulfilled(registers) {
             registers.PC = memory.get_u16(registers.SP.into());
             registers.SP += 2;
@@ -439,16 +332,6 @@ impl Instruction for RetIf {
         } else {
             Ok(2)
         }
-    }
-
-    fn length(&self) -> u16 {
-        1
-    }
-}
-
-impl Display for RetIf {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "RET {:?}", self.condition)
     }
 }
 
