e4264899c18d63e25f15570e1dd96927b66c8874
iensu <jostlund@gmail.com>
2020-09-17 20:03:53 +0000

    Deploying to gh-pages from  @ 8c9682b9cb03e80505bc9a75725494de5816934a 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["gejmboj"] = {"name":"","files":["main.rs"]};
-sourcesIndex["gejmboj_cpu"] = {"name":"","dirs":[{"name":"instructions","files":["control_flow.rs","load.rs","misc.rs"]}],"files":["cpu.rs","errors.rs","instructions.rs","lib.rs","memory.rs","registers.rs"]};
+sourcesIndex["gejmboj_cpu"] = {"name":"","dirs":[{"name":"instructions","files":["control_flow.rs","load.rs","misc.rs"]}],"files":["cpu.rs","errors.rs","instructions.rs","lib.rs","macros.rs","memory.rs","registers.rs"]};
 createSourceSidebar();
