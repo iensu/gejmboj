e4f739547380bc842714736a5d05c4b2a1801f30
Jens Östlund <jostlund@gmail.com>
2021-04-02 09:51:53 +0200

    Fix into_bits function

--- gejmboj_cpu/src/instructions.rs
+++ gejmboj_cpu/src/instructions.rs
@@ -60,13 +60,13 @@ fn get_16bit_operand(pc: u16, memory: &Memory) -> u16 {
 
 fn into_bits(x: u8) -> (u8, u8, u8, u8, u8, u8, u8, u8) {
     (
-        x & 0b1000_0000,
-        x & 0b0100_0000,
-        x & 0b0010_0000,
-        x & 0b0001_0000,
-        x & 0b0000_1000,
-        x & 0b0000_0100,
-        x & 0b0000_0010,
+        (x & 0b1000_0000) >> 7,
+        (x & 0b0100_0000) >> 6,
+        (x & 0b0010_0000) >> 5,
+        (x & 0b0001_0000) >> 4,
+        (x & 0b0000_1000) >> 3,
+        (x & 0b0000_0100) >> 2,
+        (x & 0b0000_0010) >> 1,
         x & 0b0000_0001,
     )
 }
@@ -147,5 +147,25 @@ pub fn decode(opcode: u8, pc: u16, memory: &Memory) -> Result<Instruction, CpuEr
 
         // Catch all
         _ => Err(CpuError::UnknownInstruction(opcode)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn into_bits_works() {
+        assert_eq!(into_bits(0b1000_0000), (1, 0, 0, 0, 0, 0, 0, 0));
+        assert_eq!(into_bits(0b0100_0000), (0, 1, 0, 0, 0, 0, 0, 0));
+        assert_eq!(into_bits(0b0010_0000), (0, 0, 1, 0, 0, 0, 0, 0));
+        assert_eq!(into_bits(0b0001_0000), (0, 0, 0, 1, 0, 0, 0, 0));
+        assert_eq!(into_bits(0b0000_1000), (0, 0, 0, 0, 1, 0, 0, 0));
+        assert_eq!(into_bits(0b0000_0100), (0, 0, 0, 0, 0, 1, 0, 0));
+        assert_eq!(into_bits(0b0000_0010), (0, 0, 0, 0, 0, 0, 1, 0));
+        assert_eq!(into_bits(0b0000_0001), (0, 0, 0, 0, 0, 0, 0, 1));
+        assert_eq!(into_bits(0b0000_0000), (0, 0, 0, 0, 0, 0, 0, 0));
+        assert_eq!(into_bits(0b1111_1111), (1, 1, 1, 1, 1, 1, 1, 1));
+        assert_eq!(into_bits(0b1000_1000), (1, 0, 0, 0, 1, 0, 0, 0));
     }
 }
