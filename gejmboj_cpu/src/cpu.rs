29858d51f5e905c336fe37094562831afcd38ce4
Jens Östlund <jostlund@gmail.com>
2021-11-05 17:26:00 +0100

    Add IME flag handling in the CPU

--- gejmboj_cpu/src/cpu.rs
+++ gejmboj_cpu/src/cpu.rs
@@ -48,8 +48,76 @@ impl CPU {
 
         registers.PC += instruction.length();
 
+        if self.flags.IME_scheduled {
+            self.flags.IME = true;
+            self.flags.IME_scheduled = false;
+        }
+
         instruction.execute(registers, memory, &mut self.flags)?;
 
         Ok((instruction_location, instruction))
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use instructions::misc;
+    use instructions::Instruction;
+
+    #[test]
+    fn cpu_tick_executes_instructiona() {
+        let mut registers = Registers::new();
+        let mut memory = Memory::new();
+        let mut cpu = CPU::new();
+
+        let noop = 0b0000_0000;
+        memory.set_u16(0x0000, noop);
+
+        assert_eq!(0, registers.PC);
+
+        let (_, instruction) = cpu.tick(&mut registers, &mut memory).unwrap();
+
+        assert_eq!(Instruction::Misc(misc::Misc::Noop()), instruction);
+        assert_eq!(instruction.length(), registers.PC);
+    }
+
+    #[test]
+    fn cpu_tick_handles_interrupt_scheduling() {
+        let mut registers = Registers::new();
+        let mut memory = Memory::new();
+        let mut cpu = CPU::new();
+
+        let ei_op = 0b1111_1011;
+        let noop = 0b0000_0000;
+
+        memory.set_u16(0x0000, ei_op);
+        memory.set_u16(0x0002, noop);
+
+        let (_, instruction) = cpu
+            .tick(&mut registers, &mut memory)
+            .expect("Failed to execute EI instruction");
+
+        assert_eq!(Instruction::Misc(misc::Misc::EI()), instruction);
+        assert_eq!(instruction.length(), registers.PC);
+        assert_eq!(
+            CpuFlags {
+                IME: false,
+                IME_scheduled: true
+            },
+            cpu.flags
+        );
+
+        cpu.tick(&mut registers, &mut memory)
+            .expect("Failed to execute Noop instruction");
+
+        assert_eq!(
+            CpuFlags {
+                IME: true,
+                IME_scheduled: false,
+            },
+            cpu.flags
+        );
     }
 }
