0dfbacf128796d6678308a829e8749bfcf3eb78e
iensu <jostlund@gmail.com>
2022-03-26 22:21:36 +0000

    Deploying to gh-pages from @ iensu/gejmboj@6792d2fa621d74021098378941b26d66309d1b18 🚀

---
+++ gejmboj_cpu/instructions/bit/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Bit","Bit operations"]],"fn":[["decode","Decodes the `operand` into a `Bit` instruction."]]});
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["gejmboj"] = {"name":"","files":["main.rs"]};
-sourcesIndex["gejmboj_cpu"] = {"name":"","dirs":[{"name":"instructions","files":["alu_16bit.rs","alu_8bit.rs","control_flow.rs","load_16bit.rs","load_8bit.rs","misc.rs","rotate_shift.rs","utils.rs"]}],"files":["cpu.rs","errors.rs","instructions.rs","lib.rs","macros.rs","memory.rs","registers.rs"]};
+sourcesIndex["gejmboj_cpu"] = {"name":"","dirs":[{"name":"instructions","files":["alu_16bit.rs","alu_8bit.rs","bit.rs","control_flow.rs","load_16bit.rs","load_8bit.rs","misc.rs","rotate_shift.rs","utils.rs"]}],"files":["cpu.rs","errors.rs","instructions.rs","lib.rs","macros.rs","memory.rs","registers.rs"]};
 createSourceSidebar();
