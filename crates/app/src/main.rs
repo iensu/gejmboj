eba87a9e85eb7b11cd17e043ffe9f2f2bcb3c025
Jens Östlund <jostlund@gmail.com>
2026-06-01 22:04:15 +0200

    Read ROM file bytes

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+use log::debug;
+
+fn main() -> Result<(), std::io::Error> {
+    env_logger::init();
+
+    let Some(file_path) = env::args().nth(1) else {
+        panic!("You must provide a path to a Game Boy rom file!");
+    };
+    let rom_bytes = fs::read(&file_path)?;
+
+    debug!("Read {} bytes from {file_path}", rom_bytes.len());
+
+    Ok(())
 }
